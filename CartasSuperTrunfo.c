cfb7d4df3fd70d6dde21b44aed4b494434917fde
geraldsic <geraldgoncalves@gmail.com>
2025-11-21 02:44:08 +0000

    inserido as duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,33 +5,74 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+<<<<<<< HEAD
   printf("Bem vindo ao Super Trunfo");
   // Área para definição das variáveis para armazenar as propriedades das cidades
+=======
+    // Área para definição das variáveis para armazenar as propriedades das cidades
+  // VARIAVEIS PRA CARTA 1
+  char estado[2];
+  char codcarta[4];
+  char cidade[50];
+  int população;
+  float area;
+  float pib;
+  int pturisticos;
+    // VARIAVEIS PRA CARTA 2
+  char estado2[2];
+  char codcarta2[4];
+  char cidade2[50];
+  int população2;
+  float area2;
+  float pib2;
+  int pturisticos2;
+>>>>>>> f2a1dd6 (inserido as duas cartas)
 
   // Área para entrada de dados
- 
+ // ENTRADA CARTA 1
   printf("Insira Dados Da PRIMEIRA Carta: \n");
-  printf("Estado: Uma letra de 'A' a 'H'");
-  scanf("%s", &estado);
-    getchar();
-  printf("Código da Carta:");
-     scanf("%s", &codcarta);
-    getchar();
-  printf("Nome da Cidade:");
-  scanf("%s", &cidade);
-
-  printf("População:");
+  printf("Estado: ");
+  scanf("%s", estado);
+  getchar();
+  printf("Código da Carta: ");
+  scanf("%s", codcarta);
+  getchar();
+  printf("Nome da Cidade: ");
+  fgets (cidade, 50, stdin); // LINHA PROBLEMATICA 
+  printf("População: ");
   scanf("%d", &população);
-
-  printf("Área (em km²): \n");
+  getchar();
+  printf("Área (em km²): ");
   scanf("%f", &area);  
-
-  printf("PIB: \n");
+  getchar();
+  printf("PIB: ");
   scanf("%f", &pib);  
-
-  printf("Número de Pontos Turísticos: \n");
+  getchar();
+  printf("Número de Pontos Turísticos: ");
   scanf("%d", &pturisticos);  
-
+  
+  
+// ENTRADA CARTA 2
+  printf("\nInsira Dados Da SEGUNDA Carta: \n");
+  printf("Estado: ");
+  scanf("%s", estado2);
+  getchar();
+  printf("Código da Carta: ");
+  scanf("%s", codcarta2);
+  getchar();
+  printf("Nome da Cidade: ");
+  fgets (cidade2, 50, stdin);
+  printf("População: ");
+  scanf("%d", &população2);
+  getchar();
+  printf("Área (em km²): ");
+  scanf("%f", &area2);  
+  getchar();
+  printf("PIB: ");
+  scanf("%f", &pib2);  
+  getchar();
+  printf("Número de Pontos Turísticos: ");
+  scanf("%d", &pturisticos2);  
 
   // Área para exibição dos dados da cidade
 printf("Carta 1:\n");
@@ -40,8 +81,15 @@ printf("Carta: %s\n", codcarta);
 printf("Cidade: %s\n", cidade);
 printf("População: %d\n", população);
 printf("Área %.2f km²\n", area / 100.0);
-printf("PIB %.2f \n", pib / 100.0);
+printf("PIB %.2f \n\n", pib / 100.0);
 
+printf("Carta 2:\n");
+printf("Estado: %s\n", estado2); 
+printf("Carta: %s\n", codcarta2);
+printf("Cidade: %s\n", cidade2);
+printf("População: %d\n", população2);
+printf("Área %.2f km²\n", area2 / 100.0);
+printf("PIB %.2f \n", pib2 / 100.0);
 
 
 return 0;
